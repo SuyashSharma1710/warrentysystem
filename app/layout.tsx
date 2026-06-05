4f100f1a88271e9576a9cff02f6ee172abb7599e
Suyash Sharma <110248158+SuyashSharma1710@users.noreply.github.com>
2026-06-05 17:35:23 +0530

    Update metadata title and description for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'My Google AI Studio App',
-  description: 'My Google AI Studio App',
+  title: 'Warrenty Sync',
+  description: 'Warrenty Management RBAC cms',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
